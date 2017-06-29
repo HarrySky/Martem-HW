876519b1e8d977fcbd05d76e5d9e8a348fece170
Igor Nehoroshev <HarrySky@users.noreply.github.com>
2017-06-29 03:13:43 +0300

    Basic Arguments Checking Added
    Checking for at least two arguments and preparing ground for optional arguments

--- source.cpp
+++ source.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
 using namespace std;
 
-/// Hello world for start
+#define STATS 0
+#define LIST 1
+#define ALL 2
+
+#define STATS_ARG "--stats"
+#define LIST_ARG "--list"
+#define ALL_ARG "--all"
+
 int main(int argc, char **argv)
 {
-	cout << "Hello World!";
+	if (argc < 2)
+	{
+		cout << "Input file missing!";
+		cout << "Try again: " << argv[0] << " <filename>\n";
+		return -1;
+	}
+	
+	char* filename = argv[1];
+	int action = STATS;
+	if (argc > 2 && argv[2] != STATS_ARG)
+	{
+
+	}
+
 	return 0;
 }
