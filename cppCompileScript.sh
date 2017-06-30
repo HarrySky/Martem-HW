a20fff2cd71bd328acdff2b6e2bb5f96ca6880d7
Igor Nehoroshev <HarrySky@users.noreply.github.com>
2017-06-30 08:23:55 +0300

    Boost Library Arguments Added

--- cppCompileScript.sh
+++ cppCompileScript.sh
@@ -2,5 +2,5 @@
 # Not tested, but should work.
 
 printf "Compilation started\n"
-g++ source.cpp -o compiled
+g++ source.cpp -o compiled -lboost_filesystem -lboost_date_time -lboost_system
 printf "Compilation finished\n"
